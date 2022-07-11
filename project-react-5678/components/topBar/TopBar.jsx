da99e25d92a111b34676db232122d00f20bc7cbb
RedemptionC <842391412@qq.com>
2022-07-11 16:35:00 +0800

    [Refactor] put appContext on the other side of toolBar

--- project-react-5678/components/topBar/TopBar.jsx
+++ project-react-5678/components/topBar/TopBar.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import {
-  AppBar, Toolbar, Button
-} from '@material-ui/core';
-import './TopBar.css';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { AppBar, Toolbar, Button, Box } from "@material-ui/core";
+import "./TopBar.css";
+import { useLocation } from "react-router-dom";
 
 /**
  * Define TopBar, a React componment of CS142 project #5
@@ -17,15 +15,15 @@ class TopBar extends React.Component {
     return (
       <AppBar className="cs142-topbar-appBar" position="absolute">
         <Toolbar>
-          <Button  color="secondary" href="/" disabled>
+          <Box display="flex" flexGrow={1}>
+            <Button color="secondary" href="/" disabled>
               Cheng Geng
-          </Button>
-          <Button  color="inherit" href="/photo-share.html">
+            </Button>
+            <Button color="inherit" href="/photo-share.html">
               Home
-          </Button>
-          <Button  color="inherit">
-              {this.props.appContext}
-          </Button>
+            </Button>
+          </Box>
+          <Button color="inherit">{this.props.appContext}</Button>
         </Toolbar>
       </AppBar>
     );
